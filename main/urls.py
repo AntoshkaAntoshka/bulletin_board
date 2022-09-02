513cdff60dc1055c55a87d5b45be51ed1191ae4e
AntoshkaAntoshka <antongrishin1994@mail.ru>
2022-09-02 06:43:56 -0700

    Login, Logout and ProfilePage

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import index
+from .views import BBLogoutView, index
 from .views import other_page
+from .views import BBLoginView, BBLogoutView
+from .views import profile
 
 app_name = 'main'
 urlpatterns = [
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
+    path('accounts/login/', BBLoginView.as_view(), name='login'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
 ]
