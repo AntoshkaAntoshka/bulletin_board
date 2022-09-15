9fab5fb24662f8d089daedde2d17123eebba6447
AntoshkaAntoshka <antongrishin1994@mail.ru>
2022-09-15 15:43:49 -0700

    Registration_with_email_letter

--- main/forms.py
+++ main/forms.py
@@ -23,14 +23,6 @@ class RegisterUserForm(forms.ModelForm):
             password_validation.validate_password(password1)
         return password1
 
-    def clean(self):
-        super().clean()
-        password1 = self.cleaned_data['password1']
-        password2 = self.cleaned_data['password2']
-        if password1 and password2 and password1 != password2:
-            errors = {'password2': ValidationError('Введенные пароли не совпадают', code='password_mismatch')}
-        raise ValidationError(errors)
-
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data['password1'])
