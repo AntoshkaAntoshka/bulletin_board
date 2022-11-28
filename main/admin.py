d0f5edfe830a3dd726f61ea9acf4efc55c89498e
AntoshkaAntoshka <87267118+AntoshkaAntoshka@users.noreply.github.com>
2022-11-28 18:48:05 +0300

    Rubrics (#2)
    * Rubric: models
    
    * Rubric administration

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 import datetime
 
 from .models import AdvUser
+from .models import SuperRubric, SubRubric
+from .forms import SubRubricForm
 from .utilities import send_activation_notification
 
 def send_activation_notifications(modeladmin, request, queryset):
@@ -47,9 +49,19 @@ class AdvUserAdmin(admin.ModelAdmin):
 admin.site.register(AdvUser, AdvUserAdmin)    
 
 
+class SubRubricInline(admin.TabularInline):
+    model = SubRubric
 
+class SuperRubricAdmin(admin.ModelAdmin):
+    exclude = ('super_rubric',)
+    inlines = (SubRubricInline,)
 
+admin.site.register(SuperRubric, SuperRubricAdmin)
 
+class SubRubricAdmin(admin.ModelAdmin):
+    form = SubRubricForm
+
+admin.site.register(SubRubric, SubRubricAdmin)
 
 
 
